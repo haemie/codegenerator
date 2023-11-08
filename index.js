c42307dceae96624536aac819188dce5a5ac734e
haemie <jmezhang@gmail.com>
2023-11-08 14:06:10 -0600

    bugfix: fixed bug preventing alphabetical characters from working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const readline = require('readline');
 let length;
 const maybes = new Map();
@@ -10,9 +11,7 @@ const r1 = readline.createInterface({
 r1.question('how long is the code? \n', (input) => {
   // console.log(`code is  ${input} long`);
   length = input;
-  console.log(
-    'give your best guess for the character at each position, separated by spaces\n'
-  );
+  console.log('give your best guess for the character at each position\n');
   generateQuestions(length);
 });
 
@@ -22,10 +21,11 @@ function generateQuestions(remainingQuestions) {
     r1.close();
   } else {
     r1.question(`at the ${length - remainingQuestions} position\n`, (input) => {
-      const splitted = input.split(' ');
+      const splitted = input.split('');
       const nums = [];
       splitted.forEach((e) => {
-        if (/.*[0-9].*/.test(e)) nums.push(Number(e));
+        if (/[0-9]/.test(e)) nums.push(Number(e));
+        else if (/[a-zA-Z]/.test(e)) nums.push(e);
       });
       maybes.set(length - remainingQuestions, nums);
       generateQuestions(remainingQuestions - 1);
@@ -36,7 +36,10 @@ function generateQuestions(remainingQuestions) {
 function generatePossibilities(currCode = '', index = 0) {
   // console.log(maybes);
   if (index === maybes.size) {
-    console.log(currCode);
+    fs.appendFile('guesses.txt', `${currCode}\n`, (err) => {
+      if (err) throw err;
+      console.log(currCode);
+    });
   } else {
     if (!Array.isArray(maybes.get(index))) {
       generatePossibilities(currCode + maybes.get(index), index + 1);
@@ -65,7 +68,7 @@ function generatePossibilities(currCode = '', index = 0) {
 // certain[14] = 8;
 // certain[15] = 'S';
 
-let currCode = '';
+// let currCode = '';
 // for (let i = 0; i < certain.length; i++) {
 //   if (certain[i] !== undefined) {
 //     maybes.set(i, certain[i]);
